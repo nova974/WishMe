03c4455ed21882082937009a8f051fda548037c3
William GOSSARD <gossard.william@gmail.com>
2019-01-11 08:23:54 +0100

    Add new wish model

--- Wishlist/Models/WishModel.swift
+++ Wishlist/Models/WishModel.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+import RealmSwift
+
+class Wish: Object {
+    
+    @objc dynamic var url: String?
+    @objc dynamic var finalUrl: String?
+    @objc dynamic var canonicalUrl: String?
+    @objc dynamic var title: String?
+    @objc dynamic var desc: String?
+    @objc dynamic var image: String?
+    @objc dynamic var icon: String?
+    @objc dynamic var price: String?
+}
