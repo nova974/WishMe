715cb3d47381d0a452ccfaff8f335a96fe36cc1d
William GOSSARD <gossard.william@gmail.com>
2019-01-11 08:26:14 +0100

    Add new image as bg on homecell

--- Wishlist/AppDelegate.swift
+++ Wishlist/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,10 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        // Setting main view
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        
         let layout = UICollectionViewFlowLayout()
         window?.rootViewController = UINavigationController(rootViewController: HomeController(collectionViewLayout: layout))
         
--- Wishlist/Home/Views/WishCell.swift
+++ Wishlist/Home/Views/WishCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class WishCell: UICollectionViewCell {
     
-    private let titleLabel: UILabel = {
+    private var titleLabel: UILabel = {
         var l = UILabel()
         l.text = "title yeah"
         l.textColor = .black
@@ -18,12 +18,41 @@ class WishCell: UICollectionViewCell {
         return l
     }()
     
+    private lazy var bgImageView: UIImageView = {
+        var iv = UIImageView()
+        iv.image = UIImage(named: "pandaroux")
+        iv.clipsToBounds = true
+        iv.contentMode = .scaleAspectFill
+        
+        iv.addSubview(overlay)
+
+        return iv
+    }()
+    
+    private lazy var overlay: UIView = {
+        var v = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height))
+        v.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.4)
+        return v
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        setupView()
         
+    }
+
+    private func setupView() {
+        
+        addSubview(bgImageView)
         addSubview(titleLabel)
-        
+
+        bgImageView.translatesAutoresizingMaskIntoConstraints = false
+        bgImageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        bgImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        bgImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        bgImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+
         titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 20).isActive = true
         titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 12).isActive = true
         titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
