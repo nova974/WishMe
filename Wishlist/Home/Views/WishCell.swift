83513393e0544126aaac0aa7347d52b62dbdeae1
William GOSSARD <gossard.william@gmail.com>
2019-01-14 10:41:56 +0100

    Add label for price & title

--- Wishlist/Home/Views/WishCell.swift
+++ Wishlist/Home/Views/WishCell.swift
@@ -12,9 +12,20 @@ class WishCell: UICollectionViewCell {
     
     private var titleLabel: UILabel = {
         var l = UILabel()
-        l.text = "title yeah"
+        l.text = "Title yeah"
         l.textColor = .black
+        l.font = UIFont.boldSystemFont(ofSize: 24)
         l.translatesAutoresizingMaskIntoConstraints = false
+        return l
+    }()
+
+    private var priceLabel: UILabel = {
+        var l = UILabel()
+        l.text = "200 €"
+        l.textColor = .black
+        l.font = UIFont.boldSystemFont(ofSize: 18)
+        l.translatesAutoresizingMaskIntoConstraints = false
+
         return l
     }()
     
@@ -31,7 +42,7 @@ class WishCell: UICollectionViewCell {
     
     private lazy var overlay: UIView = {
         var v = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height))
-        v.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.4)
+        v.backgroundColor = UIColor(red: 127/255, green: 127/255, blue: 127/255, alpha: 0.5)
         return v
     }()
     
@@ -46,6 +57,7 @@ class WishCell: UICollectionViewCell {
         
         addSubview(bgImageView)
         addSubview(titleLabel)
+        addSubview(priceLabel)
 
         bgImageView.translatesAutoresizingMaskIntoConstraints = false
         bgImageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
@@ -53,10 +65,14 @@ class WishCell: UICollectionViewCell {
         bgImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
         bgImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
 
-        titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 20).isActive = true
-        titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 12).isActive = true
-        titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-        titleLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        titleLabel.bottomAnchor.constraint(equalTo: priceLabel.topAnchor).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: priceLabel.leadingAnchor).isActive = true
+        titleLabel.widthAnchor.constraint(equalToConstant: self.frame.width).isActive = true
+
+        priceLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12).isActive = true
+        priceLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 12).isActive = true
+        priceLabel.widthAnchor.constraint(equalToConstant: self.frame.width).isActive = true
+
     }
     
     required init?(coder aDecoder: NSCoder) {
